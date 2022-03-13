64b920fa5164632ac721f1923ef91c07e477fc29
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-13 12:59:39 +0530

    [Wasim] Add: Handle Exception if Indivalid Mood

---
+++ MoodAnalyzer/MoodAnalyser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyser
+    {
+        public string AnalyzeMood(string moodMessage)
+        {
+            try
+            {
+                if (moodMessage == null)
+                    throw new NullReferenceException();
+                if (moodMessage.ToLower().Contains("sad"))
+                    return "sad";
+                else
+                    return "happy";
+            }
+            catch (Exception ex)
+            {
+                return "happy";
+            }
+        }
+    }
+}
