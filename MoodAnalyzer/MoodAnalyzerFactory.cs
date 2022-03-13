56e4d3a44cc918d583200827b440e5ed1cf23319
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-13 13:50:58 +0530

    [Wasim] Add: MoodAnalyzer using Parameterized Constructer

--- MoodAnalyzer/MoodAnalyzerFactory.cs
+++ MoodAnalyzer/MoodAnalyzerFactory.cs
@@ -32,5 +32,24 @@ namespace MoodAnalyzer
                 return new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_METHOD, "No Such Method");
             }
         }
+        public static object CreateMoodAnalyserUsingParameterizedConstructer(string classname, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.Name.Equals(classname) || type.FullName.Equals(classname))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo cons = type.GetConstructor(new Type[] { typeof(string) });
+                    object instance = cons.Invoke(new object[] { message });
+                    return instance;
+                }
+                else
+                    return new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor Not Found");
+            }
+            else
+            {
+                return new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_CLASS, "No Such Class");
+            }
+        }
     }
 }
