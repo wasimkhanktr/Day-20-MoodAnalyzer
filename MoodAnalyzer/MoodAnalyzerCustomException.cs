33f35036cf631610526f969e4bea71964ff5097e
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-13 13:43:21 +0530

    [Wasim] Add: MoodAnalyzer using Reflection

---
+++ MoodAnalyzer/MoodAnalyzerCustomException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyzerCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_MESSAGE_EXCEPTION, EMPTY_MESSAGE_EXCEPTION,
+            NO_SUCH_CLASS,
+            NO_SUCH_METHOD
+        }
+        private readonly ExceptionType exceptionType;
+        public MoodAnalyzerCustomException(ExceptionType exceptionType, string message) : base(message)
+        {
+            this.exceptionType = exceptionType;
+        }
+    }
+}
---
+++ MoodAnalyzer/MoodAnalyzerFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyzerFactory
+    {
+        public static object CreateMoodAnalyser(string classname, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(classname, pattern);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodanalysis = executing.GetType(classname);
+                    return Activator.CreateInstance(moodanalysis);
+                }
+                catch (ArgumentNullException)
+                {
+                    return new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_CLASS, "No such Class");
+                }
+            }
+            else
+            {
+                return new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_METHOD, "No Such Method");
+            }
+        }
+    }
+}
