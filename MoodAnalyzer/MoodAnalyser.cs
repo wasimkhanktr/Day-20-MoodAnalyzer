0bd4f2c77cc2def4b8175cbbae461007e151fa5f
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-13 12:28:43 +0530

    [Wasim] Add: Mood Analyser to analyse the mood is sad or happy

---
+++ MoodAnalyzer/MoodAnalyser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyser
+    {
+        public string AnalyzeMood(string moodMessage)
+        {
+            if (moodMessage.ToLower().Contains("sad"))
+                return "sad";
+            else
+                return "happy";
+        }
+    }
+}
